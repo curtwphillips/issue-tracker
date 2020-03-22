7bcbe5aecb605f8b4e777692a48b0f0df3ef98df
curtwphillips <curtwphillips@gmail.com>
2020-03-22 11:28:54 -0500

    remove static middleware from API server

--- api/server.js
+++ api/server.js
@@ -93,8 +93,6 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.static('public'));
-
 server.applyMiddleware({ app, path: '/graphql'});
 
 async function connectToDb() {
@@ -108,7 +106,7 @@ async function connectToDb() {
     try {
         await connectToDb();
         app.listen(3000, function () {
-            console.log('App started on port 3000');
+            console.log('API server started on port 3000');
         });
     } catch (err) {
         console.log('Error:', err);
