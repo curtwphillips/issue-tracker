9a9594a20fa60c14ce45702265c885b64e801b2a
curtwphillips <curtwphillips@gmail.com>
2020-03-22 15:39:25 -0500

    add eslint to api

--- api/scripts/trymongo.js
+++ api/scripts/trymongo.js
@@ -1,52 +1,47 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
 const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 
 function testWithCallbacks(callback) {
-    console.log('\n--- testWithCallbacks ---');
-    const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-    client.connect((err, client) => {
-        if (err) {
-            callback(err);
+  console.log('\n--- testWithCallbacks ---');
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect((connErr) => {
+    if (connErr) {
+      callback(connErr);
+      return;
+    }
+    const db = client.db();
+    const collection = db.collection('employees');
+    const employee = { id: 1, name: 'A. Callback', age: 23 };
+    collection.insertOne(employee, (insertErr, result) => {
+      if (insertErr) {
+        client.close();
+        callback(insertErr);
+        return;
+      }
+      console.log('Result of insert:\n', result.insertedId);
+
+      collection.find({ _id: result.insertedId })
+        .toArray((findErr, docs) => {
+          if (findErr) {
+            client.close();
+            callback(findErr);
             return;
-        }
-        const db = client.db();
-        const collection = db.collection('employees');
-        const employee = {id: 1, name: 'A. Callback', age: 23};
-        collection.insertOne(employee, (err, result) => {
-            if (err) {
-                client.close();
-                callback(err);
-                return;
-            }
-            console.log('Result of insert:\n', result.insertedId);
-    
-            collection.find({_id: result.insertedId})
-                .toArray((err, docs) => {
-                    if (err) {
-                        client.close();
-                        callback(err);
-                        return;
-                    }
-                    console.log('Result of find:\n', docs);
-                    client.close();
-                    callback();
-                })
-        })
+          }
+          console.log('Result of find:\n', docs);
+          client.close();
+          callback();
+        });
     });
+  });
 }
 
-testWithCallbacks((err) => {
-    if (err) console.log(err);
-    testWithAsync();
-})
-
 async function testWithAsync() {
-    console.log('\n--- testWithAsync ---');
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
+  console.log('\n--- testWithAsync ---');
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
     await client.connect();
     console.log('Connected to MongoDB URL', url);
     const db = client.db();
@@ -55,11 +50,16 @@ async function testWithAsync() {
     const result = await collection.insertOne(employee);
     console.log('Result of insert:\n', result.insertedId);
     const docs = await collection.find({ _id: result.insertedId })
-.toArray();
-console.log('Result of find:\n', docs);
-} catch(err) {
-console.log(err);
-} finally {
-client.close();
+      .toArray();
+    console.log('Result of find:\n', docs);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
 }
-}
+
+testWithCallbacks((err) => {
+  if (err) console.log(err);
+  testWithAsync();
+});
--- api/server.js
+++ api/server.js
@@ -12,109 +12,114 @@ const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 let db;
 
 async function getNextSequence(name) {
-    const result = await db.collection('counters').findOneAndUpdate(
-        {_id: name},
-        {$inc: {current: 1}},
-        {returnOriginal: false},
-    );
-    return result.value.current;
+  const result = await db.collection('counters').findOneAndUpdate(
+    { _id: name },
+    { $inc: { current: 1 } },
+    { returnOriginal: false },
+  );
+  return result.value.current;
 }
 
 function validateIssue(issue) {
-    const errors = [];
-    if (issue.title.length < 3) {
-        errors.push('Field "title" must be at least 3 characters long.');
-    }
-    if (issue.status == 'Assigned' && !issue.owner) {
-        errors.push('Field "owner" is required when status is "Assigned"');
-    }
-    if (errors.length > 0) {
-        throw new UserInputError('Invalid input(s)', { errors });
-    }
+  const errors = [];
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be at least 3 characters long.');
+  }
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"');
+  }
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors });
+  }
 }
 
 const GraphQLDate = new GraphQLScalarType({
-    name: 'GraphQLDate',
-    description: 'A Date() type in GraphQL as a scalar',
-    parseValue(value) {
-        const dateValue = new Date(value);
-        return isNaN(dateValue) ? undefined : dateValue;
-    },
-    parseLiteral(ast) {
-        if (ast.kind == Kind.STRING) {
-            const value = new Date(ast.value);
-            return isNaN(value) ? undefined : value;
-        }
-    },
-    serialize(value) {
-        return value.toISOString();
+  name: 'GraphQLDate',
+  description: 'A Date() type in GraphQL as a scalar',
+  parseValue(value) {
+    const dateValue = new Date(value);
+    const response = Number.isNaN(dateValue.getTime()) ? undefined : dateValue;
+    return response;
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.STRING) {
+      const value = new Date(ast.value);
+      const response = Number.isNaN(value.getTime()) ? undefined : value;
+      return response;
     }
+    return undefined;
+  },
+  serialize(value) {
+    return value.toISOString();
+  },
 });
 
-async function issueAdd(_, {issue}) {
-    console.log('issueAdd:\n', issue);
-    validateIssue(issue);
-    issue.created = new Date();
-    issue.id = await getNextSequence('issues');
-    const result = await db.collection('issues').insertOne(issue);
-    const savedIssue = await db.collection('issues')
-        .findOne({_id: result.insertedId});
-    return savedIssue;
+async function issueAdd(_, { issue }) {
+  console.log('issueAdd:\n', issue);
+  validateIssue(issue);
+  const newIssue = Object.assign({}, issue);
+  newIssue.created = new Date();
+  newIssue.id = await getNextSequence('issues');
+  const result = await db.collection('issues').insertOne(newIssue);
+  const savedIssue = await db.collection('issues')
+    .findOne({ _id: result.insertedId });
+  return savedIssue;
 }
 
-let aboutMessage = "Issue Tracker API v1.0";
-
-const resolvers = {
-    Query: {
-        about: () => aboutMessage,
-        issueList,
-    },
-    Mutation: {
-        issueAdd,
-        setAboutMessage,
-    },
-    GraphQLDate
-};
-
-async function issueList() {
-    const issues = await db.collection('issues').find({}).toArray();
-    return issues;
-}
+let aboutMessage = 'Issue Tracker API v1.0';
 
 function setAboutMessage(_, { message }) {
-    return aboutMessage = message;
+  aboutMessage = message;
+  return aboutMessage;
 }
 
+async function issueList() {
+  const issues = await db.collection('issues').find({}).toArray();
+  return issues;
+}
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+    issueList,
+  },
+  Mutation: {
+    issueAdd,
+    setAboutMessage,
+  },
+  GraphQLDate,
+};
+
 const server = new ApolloServer({
-    typeDefs: fs.readFileSync('./schema.graphql', 'utf-8'),
-    resolvers,
-    formatError: error => {
-        console.log(JSON.stringify(error));
-        return error;
-    }
+  typeDefs: fs.readFileSync('./schema.graphql', 'utf-8'),
+  resolvers,
+  formatError: (error) => {
+    console.log(JSON.stringify(error));
+    return error;
+  },
 });
 
 const app = express();
 
-const enableCors = (process.env.ENABLE_CORS || 'true') == 'true';
+const enableCors = (process.env.ENABLE_CORS || 'true') === 'true';
 console.log('CORS setting:', enableCors);
 
-server.applyMiddleware({ app, path: '/graphql', cors: enableCors});
+server.applyMiddleware({ app, path: '/graphql', cors: enableCors });
 
 async function connectToDb() {
-    const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-    await client.connect();
-    console.log('Connected to MongoDB at', url);
-    db = client.db();
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  console.log('Connected to MongoDB at', url);
+  db = client.db();
 }
 
-(async function () {
-    try {
-        await connectToDb();
-        app.listen(port, function () {
-            console.log(`API server started on port ${port}`);
-        });
-    } catch (err) {
-        console.log('Error:', err);
-    }
-})();
+(async function start() {
+  try {
+    await connectToDb();
+    app.listen(port, () => {
+      console.log(`API server started on port ${port}`);
+    });
+  } catch (err) {
+    console.log('Error:', err);
+  }
+}());
